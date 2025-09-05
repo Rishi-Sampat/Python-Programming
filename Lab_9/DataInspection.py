5b70b528b5e395a2fe9029af4e4b6226aa627572
Rishi-Sampat <rishisampat911@gmail.com>
2025-09-06 01:14:12 +0530

    Lab_9

--- Lab_9/DataInspection.py
+++ Lab_9/DataInspection.py
@@ -1,5 +1,5 @@
 import pandas as pd
-dat = pd.read_csv("G:\sem-3\python_lab\lab9\data.csv")
+dat = pd.read_csv("E:\College\Python Programming\Lab_9\data.csv")
 print(dat.info())
 print(dat.head())
 print(dat.tail())
